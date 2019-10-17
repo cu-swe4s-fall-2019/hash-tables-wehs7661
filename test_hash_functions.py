bb5804d0e5f0636767ad44f0fe2fa91727a2e6c5
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-10-17 11:27:24 -0600

    Developed test_hash_ascii

---
+++ test_hash_functions.py
@@ -0,0 +1,14 @@
+import unittest
+import hash_functions
+
+class TestHashFunctions(unittest.TestCase):
+    def test_hash_ascii(self):
+        a = hash_functions.h_ascii('ABC', 100)
+        b = hash_functions.h_ascii('ACB', 100)
+        c = hash_functions.h_ascii('ABC', 'test')
+        self.assertEqual(a, 98)
+        self.assertEqual(a, b)
+        self.assertIsNone(c)
+
+if __name__ == '__main__':
+    unittest.main()
